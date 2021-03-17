84a06a4bc8777fd7ea878ea5d0ba84e89127088a
Milagros Rojas <milagrosrojas097@gmail.com>
2021-03-17 17:28:54 -0300

    Calculadora
    aa

---
+++ Prueba.py
@@ -0,0 +1,26 @@
+from main import *
+
+def menu():
+    calc1 = Calc()
+    while (True):
+        calc1.menu()
+        op = int(input('Introduce tu opcion:'))
+        if (op == 0):
+            break
+        else:
+            x = int(input('ingresa el primer numero: '))
+            y = int(input('ingresa el segundo numero: '))
+            if (op == 1):
+                print(calc1.sumar(x,y))
+            elif (op == 2):
+                print(calc1.resta(x, y))
+            elif (op == 3):
+                print(calc1.multi(x, y))
+            elif (op == 4):
+                print(calc1.div(x,y))
+
+if __name__ ==  '__main__':
+    menu()
+
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+
+class Calc:
+    def sumar ( self, x , y):
+            return (x + y)
+
+    def resta (self, x , y):
+           return (x - y)
+
+    def multi (self,x , y):
+            return (x * y)
+
+    def div (self, x , y):
+            return(x / y)
+
+    def menu(self):
+        print('')
+        print(' Selecciona una opcion ')
+        print(' 1- Suma')
+        print(' 2- Resta')
+        print(' 3- Multiplicacion')
+        print(' 4- Dividir')
+        print(' 0- Salir')
